8d402496a33330fa05b62e2592f35fa1d62c9506
Kinjalgiri Goswami <kinjalgirig@gmail.com>
2023-12-29 15:23:30 +0530

    movie-list

--- src/models/movie.js
+++ src/models/movie.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const movieSchema = new mongoose.Schema({
-  // userid: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: "User",
-  //   required: true,
-  // },
+  userid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
   title: {
     type: String,
     required: true,
